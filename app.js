d7657cdf4ffd8b1e6ab539e6b1fada6ad932171f
Lam Nguyen <lamnguyen@rrcs-74-142-65-90.central.biz.rr.com>
2020-08-07 15:21:31 -0400

    new

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-app.get("/",function(req,res){
-	res.send("Hi there");
-});
+
 mongoose.connect("mongodb://localhost/restful_blog_app");
-app.set("view engine" , "ejs");
-app.use(express.static("public"));
+//app.set("views', './views");
+app.set("view engine", "ejs");
+app.use(express.static("views"));
 app.use(bodyParser.urlencoded({extended: true}));
 var blogSchema = new mongoose.Schema({
 	title:String,
@@ -16,6 +15,19 @@ var blogSchema = new mongoose.Schema({
 	created: {type:Date, default:Date.now}
 });
 var Blog = mongoose.model("Blog",blogSchema);
+app.get('/', function (req, res) {
+	res.redirect("/blogs");
+  })
+app.get("/blogs", function(req,res){
+	Blog.find({},function(err, blogs){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render("index",{blogs: blogs})
+		}
+	});
+});
 app.listen(3000,function(req,res){
 	console.log("server is on"); 
 })
